f9d194e30f2676bb545998e7de11751f8e487b02
Jason Horsman <jason.horsman@brilligent.com>
2017-07-10 20:40:29 -0400

    Addin Day 1 Hello World tutorial example iOS 10 app for iPhoneSE.

---
+++ jaypr1me/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jason Horsman on 7/10/17.
+//  Copyright © 2017 JayPr1me Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
