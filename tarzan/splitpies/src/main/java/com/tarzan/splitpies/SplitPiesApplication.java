4eefc73c9ca1033968d2040a2cd760bce3c65913
tarzaninajar <rybread@gmail.com>
2017-07-30 23:04:42 -0400

    empty spring boot application to start SplitPies with

---
+++ tarzan/splitpies/src/main/java/com/tarzan/splitpies/SplitPiesApplication.java
@@ -0,0 +1,12 @@
+package com.tarzan.splitpies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitPiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitPiesApplication.class, args);
+	}
+}
