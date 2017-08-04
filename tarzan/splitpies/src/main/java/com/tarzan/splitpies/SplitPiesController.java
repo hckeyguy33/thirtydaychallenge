37c05326260880b9f6486467a3739a073a2f0e56
tarzaninajar <rybread@gmail.com>
2017-08-03 21:08:10 -0400

    Setting up some files to make a website hehe

---
+++ tarzan/splitpies/src/main/java/com/tarzan/splitpies/SplitPiesController.java
@@ -0,0 +1,13 @@
+package com.tarzan.splitpies;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SplitPiesController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
