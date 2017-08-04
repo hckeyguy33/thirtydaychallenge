6c1a5656a4d926e583fc9b2549a3441d3750b438
tarzaninajar <rybread@gmail.com>
2017-08-03 22:18:55 -0400

    plain cheese pizza, uncut

--- tarzan/splitpies/src/main/resources/static/js/pizza.js
+++ tarzan/splitpies/src/main/resources/static/js/pizza.js
@@ -1,4 +1,18 @@
-function test(){
-    console.log("Working");
+var pizza;
+
+function getPlainPizza(renderer){
+    var circle = new Graphics();
+    circle.beginFill(0xFFFF00);
+    var x = renderer.view.width / 2;
+    var y = renderer.view.height / 2;
+    var radius = renderer.view.width / 2;
+    circle.drawCircle(x, y, radius);
+    circle.endFill();
+    return circle;
 }
 
+function drawPizza(renderer, stage){
+    pizza = getPlainPizza(renderer);
+    stage.addChild(pizza);
+    renderer.render(stage);
+}
